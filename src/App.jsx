0f1fa26f7e82f4471359e78bcb60baafb80073c2
Amir Suhel <amirsuhel88@gmail.com>
2024-11-16 13:50:16 +0530

    skills are added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { NavBar } from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <>
       <NavBar />
       <Banner />
+      <Skills />
     </>
   );
 }
